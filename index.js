468770515483fb68bd3e91891357710e04ed6325
chien.pham <chien.pham@spiraledge.com>
2022-12-05 14:02:35 +0700

    complete config

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const express = require("express");
-const db = require("./src/config/db");
-const testRoute = require('./src/routes/test')
+const express = require('express');
+const db = require('./config/db');
+const testRoute = require('./routes/test');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
 db.connect((err) => {
-  err ? console("err: ", err) : console.log("Connected DB!");
+    err ? console('err: ', err) : console.log('Connected DB!');
 });
 
-db.query('select * from test', (err, result) => { err && (console.log('err')); console.log(result) })
+db.query('select * from test', (err, result) => {
+    err && console.log('err');
+    console.log(result);
+});
 
-app.use('/api/', testRoute)
-
-// app.use((req, res) => {
-//   res.send("Hello word!");
-// });
+app.use('/api/', testRoute);
 
 app.listen(port, () => {
-  console.log("Express are running on port: ", port);
+    console.log('Express are running on port: ', port);
 });
--- routes/test.js
+++ routes/test.js
@@ -1,20 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const HttpStatus = require("http-status-codes");
+const HttpStatus = require('http-status-codes');
 
-const db = require("../config/db");
+const db = require('../config/db');
 
-router.get("/test", async (req, res) => {
-  try {
-    const sql = `SELECT * FROM test`;
-    const data = await db.query(sql);
-    res.json(data);
-  } catch (err) {
-    console.log("err", err);
-    res
-      .status(HttpStatus.INTERNAL_SERVER_ERROR)
-      .send({ error: err, message: err.message });
-  }
+router.get('/test', async (req, res) => {
+    try {
+        const sql = `SELECT * FROM test`;
+        db.query(sql, function (err, result) {
+            if (err) {
+                console.log('err', err);
+                return;
+            }
+            res.json(result);
+        });
+    } catch (err) {
+        console.log('err', err);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ error: err, message: err.message });
+    }
 });
 
 module.exports = router;
